48fdae137b3224e60c26140309d020d778de057a
Michal Tynik <michaltynikt@gmail.com>
2023-10-19 14:57:28 +0200

    2023-10-19-pokracovanie

--- 2023-10-19-8/src/App.java
+++ 2023-10-19-8/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("  J    A   V     V  A");
+        System.out.println("  J   A A   V   V  A A");
+        System.out.println("  J  AAAAA   V V  AAAAA");
+        
     }
 }
