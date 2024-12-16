8e7c17a48867a0164a0e7aad6ccb4ada44613366
MichalTynik <michaltynikt@gmail.com>
2024-12-16 12:32:16 +0100

    pred Megom

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MojLinkedList mojlist = new MojLinkedList();
+            Console.WriteLine(mojlist);
+            mojlist.Append(123);
+            Console.WriteLine(mojlist);
+            mojlist.Append(456);
+            Console.WriteLine(mojlist);
+            mojlist.Append(789);
+            Console.WriteLine(mojlist);
+            Console.WriteLine(mojlist.Length());
+            Console.WriteLine(mojlist.GetValue(1));
+            Console.WriteLine(mojlist.GetValue(-2));
+            mojlist.SetValue(0, 555);
+            Console.WriteLine(mojlist);
+            mojlist.DeleteAt(0);
+            Console.WriteLine(mojlist);
+            mojlist.InsertAfter(0, 444);
+            Console.WriteLine(mojlist);
+        }
+    }
+}
